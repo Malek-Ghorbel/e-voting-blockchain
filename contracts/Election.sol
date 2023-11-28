24c516b119c5b32d1c3701debe5613036c55a96a
Malek-Ghorbel <malek.ghorbel@insat.ucar.tn>
2023-11-28 20:22:07 +0100

    e-voting project with ethereum

---
+++ contracts/Election.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.12;
+
+contract Election {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
+    event votedEvent(uint indexed _candidateId);
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "You have already voted.");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate ID.");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+        emit votedEvent(_candidateId);
+    }
+}
